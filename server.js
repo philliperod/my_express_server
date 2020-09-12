d0ab4d61aaec5a75e7542eb1716853729b25846f
Phillipe Rodriguez <philliperod@gmail.com>
2020-09-12 18:43:49 -0500

    installed nodemon and add other pages to test nodemon

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(request, response) {
-  response.send('Here I am');
+  response.send('<h1>Hello, World<h1>');
+});
+
+app.get('/contact', function(request, response) {
+  response.send('Contact me at phillipe@email.com');
+});
+
+app.get('/about', function(request, response) {
+  response.send('Phillipe is a full stack developer');
+});
+
+app.get('/hobbies', function(request, response) {
+  response.send('<ul><li>Coding<li>Listening to Music<li>Working Out<ul>');
 });
 
 app.listen(3000, function() {
